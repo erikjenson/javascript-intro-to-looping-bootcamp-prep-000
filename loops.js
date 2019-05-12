a4cb5393efded75bb2b876516ece93fa877245e2
Erik Jenson <erik.a.jenson@gmail.com>
2019-05-12 20:30:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++){
   if (i===1) {
-    array["I am 1 strange loop."];
+    array['I am 1 strange loop.'];
   }
   else {array[`I am ${i} strange loops.`];
   }
