3b8dad4e0a9b95d42a24b9ff852a371a739ce802
Erik Jenson <erik.a.jenson@gmail.com>
2019-05-12 20:36:49 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ function forLoop(a){
   if (i===1) {
     a.push('I am 1 strange loop.');
   }
-  else {a[`I am ${i} strange loops.`];
+  else {a.push(`I am ${i} strange loops.`);
   }
   return a;
 }
