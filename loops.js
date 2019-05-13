ee3abe8171922116100538c491d4dbbbd2e68442
Erik Jenson <erik.a.jenson@gmail.com>
2019-05-13 00:59:03 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,11 +18,14 @@ function whileLoop(b){
   return 'done';
 }
 
-
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
 
 function doWhileLoop(num){
   do {
     console.log("I run once regardless.");
   } 
-  while (incrementVariable(++num) < num);
+  while (incrementVariable() < num);
 }
